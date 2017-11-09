e92d638022599e2e1653596d98e866078cc55974
Peter Haley <pblhaley@gmail.com>
2017-11-09 17:47:29 -0600

    homepage routes and controller set up

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "home#index"
+
+  get '/searches' => "searches#show"
+  post '/searches' => "searches#show"
 end
