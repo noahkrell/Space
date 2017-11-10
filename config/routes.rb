a73b74170701723fd42bb9c8a79642f9c5d5c7bd
Peter Haley <pblhaley@gmail.com>
2017-11-09 18:15:02 -0600

    cleaning up after first standup

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-  
+
   resources :users
 
   root "home#index"
 
   resources :spaces
-  
+
 end
