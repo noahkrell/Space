ce3194e4018983bdfdd94535cb263672191312c6
Noah Krell <noahkrell14@gmail.com>
2017-11-12 14:20:02 -0600

    add booking route and ajax call from calendar picker. hits route correctly

--- app/assets/javascripts/date-picker.js
+++ app/assets/javascripts/date-picker.js
@@ -1,20 +1,40 @@
 $( document ).ready(function() {
 
   (function ($) {
-    $("#day-schedule").dayScheduleSelector({
-      
-      days: [1, 2, 3, 5, 4, 5, 6],
+
+    $("#day-schedule").dayScheduleSelector({  
+      days: [0, 1, 2, 3, 4, 5, 6],
       interval: 60,
       startTime: '09:00',
-      endTime: '20:00'
-      
+      endTime: '20:00'  
     });
+
     $("#day-schedule").on('selected.artsy.dayScheduleSelector', function (e, selected) {
       console.log(selected);
+      debugger
+      $.ajax({
+        url: "/book",
+        type: "POST",
+        data: { data: "hello" }
+      });
     })
+
     $("#day-schedule").data('artsy.dayScheduleSelector').deserialize({
       '0': [['09:30', '11:00'], ['13:00', '16:30']]
     });
+
   })($);
 
 });
+
+var d = new Date();
+var weekday = new Array(7);
+weekday[0] =  "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+var n = weekday[d.getDay()];
---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,33 @@
+class BookingsController < ApplicationController
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+    # start_time = 
+    # end_time = 
+    binding.pry
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def booking_params
+    # params.require(:booking).permit(:)
+  end
+
+end
