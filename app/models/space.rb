105929a3adb82aae839d80377ab06c2cfbc427d1
Peter Haley <pblhaley@gmail.com>
2017-11-13 15:37:17 -0600

    got half stars working

--- app/models/space.rb
+++ app/models/space.rb
@@ -15,6 +15,22 @@ class Space < ApplicationRecord
   pg_search_scope :search_by_address, :against => [:city, :state, :country]
 
   def average_rating
-    self.space_ratings.sum(:score) / space_ratings.size
+    bottom = self.space_ratings.size.to_f
+    top = self.space_ratings.sum(:score).to_f
+    average = top/bottom
+    binding.pry
+    return average
   end
 end
+
+
+# def average_spud_score
+#     review_count = self.reviews.count
+#     score_total = self.reviews.map(&:spud_score).inject(0, &:+)
+#     if review_count > 0
+#       average = (score_total / review_count).to_f
+#     else
+#       0
+#     end
+#     return average
+#   end
