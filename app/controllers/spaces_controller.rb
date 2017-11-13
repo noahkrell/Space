aa907104ee6e8977dee9dcacc1fbe319c5c575fc
Peter Haley <pblhaley@gmail.com>
2017-11-13 17:33:28 -0600

    got reviews to display

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -4,17 +4,14 @@ class SpacesController < ApplicationController
     if params[:search] == nil
       @spaces = Space.all
       @map_markers = Space.near(request.location)
-      binding.pry
     else
       @spaces = Space.search_by_address(params[:search])
       @map_markers = Space.near(params[:search])
-      # binding.pry
     end
   end
 
   def show
     @space = Space.find(params[:id])
-    # @comment
   end
 
 end
