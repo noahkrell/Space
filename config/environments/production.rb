bcdb566701d5f5b184514603fdc1abb81e3f6968
Noah Krell <noahkrell14@gmail.com>
2017-11-13 15:37:13 -0600

    bnlegh

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,7 +103,7 @@ Rails.application.configure do
   # Paperclip::Attachment.default_options[:s3_region] = 'us-east-2'
   # Paperclip::Attachment.default_options[:s3_host_name] = 'dbc-space-app.s3.amazonaws.com'
   Paperclip::Attachment.default_options[:s3_region] = 'us-east-2'
-  Paperclip::Attachment.default_options[:s3_host_name] = 's3.amazonaws.com'
+  Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-east-2.amazonaws.com'
   Paperclip.options[:command_path] = 'usr/local/bin'
 end
 
