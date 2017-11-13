a9e4b4d3ce73495a4aad5cf95078a8a4a4672a1c
Catherine Valek <catherine.valek@gmail.com>
2017-11-13 12:32:25 -0600

    sync aws with paperclip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  Paperclip.options[:command_path] = "/usr/local/bin/convert"
+  # Paperclip.options[:command_path] = "/usr/local/bin/convert"
 
   config.paperclip_defaults = {
     storage: :s3,
