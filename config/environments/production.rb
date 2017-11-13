f0a1f317ae68e2e9cc6ce05339e6d81e88b4b7b9
Noah Krell <noahkrell14@gmail.com>
2017-11-13 15:01:16 -0600

    adjust production defaults to test in heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,9 +100,11 @@ Rails.application.configure do
       s3_region: ENV.fetch('AWS_REGION'),
     }
   }
+  # Paperclip::Attachment.default_options[:s3_region] = 'us-east-2'
+  # Paperclip::Attachment.default_options[:s3_host_name] = 'dbc-space-app.s3.amazonaws.com'
   Paperclip::Attachment.default_options[:s3_region] = 'us-east-2'
-  Paperclip::Attachment.default_options[:s3_host_name] = 'dbc-space-app.s3.amazonaws.com'
- 
+  Paperclip::Attachment.default_options[:s3_host_name] = 's3.amazonaws.com'
+  Paperclip.options[:command_path] = 'usr/local/bin'
 end
 
 
