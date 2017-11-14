baf441d60d4c3d82df364dfe5fd613a14f461bd8
Noah Krell <noahkrell14@gmail.com>
2017-11-14 13:48:43 -0600

    more bug fixing and added some pseudocode notes

--- app/assets/javascripts/date-picker.js
+++ app/assets/javascripts/date-picker.js
@@ -8,24 +8,40 @@ $( document ).ready(function() {
       endTime: '20:00'  
     });
 
-
+    // function unsed to standardize the calendar dates. 
+    // we can find the actual dates of each subsequent day on the calendar relative to the initial sunday date (week start)
     function getSunday(d) {
       d = new Date(d);
       var day = d.getDay(),
-          diff = d.getDate() - day; // adjust when day is sunday
+          diff = d.getDate() - day;
       return new Date(d.setDate(diff));
     }
 
+    // write string function to replace value at a given index
+    // use this for the incrementHour function below
     String.prototype.replaceAt=function(index, replacement) {
     return this.substr(0, index) + replacement+ this.substr(index + replacement.length);
     }
 
+    // increments the hour by one hour 
+    // this is necessary when setting the 'end_time' in the function below
     function incrementHour(h) {
-      var plusOneHour = parseInt(h[1]) + 1
-      var newHour = plusOneHour.toString()
-      return h.replaceAt(1, newHour)
-    }
+      if (h.substring(0,2) == "19") { // accounting for booking edge case
+        var plusOneHour = parseInt(h.substring(0,2)) + 1
+        var newHour = plusOneHour.toString()
+        return h.replaceAt(0, newHour)
+      } else if (h.substring(0,2) == "09") { // accounting for booking edge case
+        var plusOneHour = parseInt(h.substring(0,2)) + 1
+        var newHour = plusOneHour.toString()
+        return h.replaceAt(0, newHour)
+      } else {
+        var plusOneHour = parseInt(h[1]) + 1
+        var newHour = plusOneHour.toString()
+        return h.replaceAt(1, newHour)
+      };
+    };
 
+    // function to pass booking data to controller to save to the db
     $("#day-schedule").on('selected.artsy.dayScheduleSelector', function (e, selected) {
       var day = $(".schedule-header th")[selected.data().day+1].innerHTML
       var start_time = selected.data().time
@@ -35,32 +51,20 @@ $( document ).ready(function() {
       var date = new Date();
       date.setDate(sunday.getDate() + selected.data().day)
       weekday = date.toDateString()
+      
       $.ajax({
         url: window.location.pathname + "/book",
         type: "POST",
         data: { booking: {day: day, date: weekday, start_time: start_time, end_time: end_time} },
         dataType: 'json'
       }).done(function(resp) {
-        // debugger
         var start = new Date(resp.booking.start_time);
         var end = new Date(resp.booking.end_time)
-        end.setHours(end.getHours() + 1);
         $("#day-schedule").hide()
         $("#booking-confirmation").show().append("Starts " + start + "<br>" + "Ends " + end);
       });
 
     })
-
-    
-    // $("#day-schedule").data('artsy.dayScheduleSelector').deserialize({
-    //   '1': [['10:00', '12:00']]
-    // });
-    // iterate through every time slot
-      // iterate through every booking
-        // if the time slot is equal to the start or end time of the booking, OR BETWEEN the start and end time of the booking
-  
-
-
   })($);
 
 });
