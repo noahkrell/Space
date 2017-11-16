bc71f48b524869e72db860cfbd3d2876a3638df3
Noah Krell <noahkrell14@gmail.com>
2017-11-16 15:32:22 -0600

    remove time conversion methods for heroku testing

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -6,13 +6,13 @@ class Booking < ApplicationRecord
 
   # booking datetimes are stored in UTC timezone in db
   # start_time and end_time are attr_reader methods that convert UTC to CT, so that every time we call it in the code it converts
-  def start_time
-    self[:start_time].in_time_zone('US/Central')
-  end
+  # def start_time
+  #   self[:start_time].in_time_zone('US/Central')
+  # end
 
-  def end_time
-    self[:end_time].in_time_zone('US/Central')
-  end
+  # def end_time
+  #   self[:end_time].in_time_zone('US/Central')
+  # end
 
 
 end
